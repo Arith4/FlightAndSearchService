7b862237cb7b0034e9ebc8c60e1c12fbeba37a2d
Arith <arithdas100@gmail.com>
2023-02-26 18:40:18 +0530

    Setup basic body-parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { port } = require("./config/serverConfig");
 
 const setUpAndStartServer = async () => {
   const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   app.get("/", (req, res) => {
     res.send("hello");
   });
